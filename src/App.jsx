52271215901d1278de7bcd5f07d5b02e568a00bf
steve2116 <s.ackers@hotmail.co.uk>
2023-06-24 15:16:39 +0100

    Initial structure

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      <h1>This is a title</h1>
-    </>
-  );
-}
-
-export default App;
---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import "../designs/App.css";
+
+function App() {
+  return (
+    <>
+      <div
+        id="background"
+        className="background"
+      />
+      <article id="intro"></article>
+      <main>
+        <section className="main" />
+      </main>
+    </>
+  );
+}
+
+export default App;
